a86d72316555f486ae7879e7ae6a19ebac8c969a
Iulian Manda <iulian.manda@gmail.com>
2020-03-03 16:44:51 +0200

    Add new test for calculator

--- lab1/test/calculator_test.cpp
+++ lab1/test/calculator_test.cpp
@@ -26,3 +26,16 @@ TEST(CalculatorTestSuite, AddNegative) {
     int expected = 0;
     ASSERT_EQ(expected, result);
 }
+
+TEST(CalculatorTestSuite, AddZero) {
+    // setup
+    int a = 1;
+    int b = 0;
+
+    // run
+    int result = Calculator::add(a, b);
+
+    // verify
+    int expected = 1;
+    ASSERT_EQ(expected, result);
+}
